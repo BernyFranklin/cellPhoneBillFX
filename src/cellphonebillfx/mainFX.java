434ac2374e6c47744f4d9958d5606a46db6a32d5
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-28 12:32:16 -0700

    Added createTop() and createLeft() methods

--- src/cellphonebillfx/mainFX.java
+++ src/cellphonebillfx/mainFX.java
@@ -56,7 +56,13 @@ public class mainFX extends Application {
     // Start start
     @Override
     public void start(Stage primaryStage) {
-        
+        // 1. Use a border pane to layout the scene on the stage
+        BorderPane root = new BorderPane();
+        root.setTop(createTop());         // Contains HBox control
+        root.setLeft(createLeft());       // Contains VBox control
+        root.setCenter(createCenter());   // Contains VBox control
+        root.setRight(createRight());     // Contains VBox control
+        root.setBottom(createBottom());   // Contains HBox control
     }   // End start
 
     // Start PSV main
@@ -64,4 +70,39 @@ public class mainFX extends Application {
         launch(args);
     }   // End PSV main
     
+    // Start createTop
+    private HBox createTop() {
+        // Puts title at top in big letters
+        // Use an HBox named hbox to hold the title. HBox used for centering
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        Font font36B = Font.font("Ariel", FontWeight.BOLD, 36); // Title font
+        lblTitle = new Label("Cell Phone Billing");
+        lblTitle.setFont(font36B);
+        hbox.getChildren().add(lblTitle);
+        return hbox;
+    }   // End of createTop
+    
+    // Start createLeft
+    private VBox createLeft() {
+        // Use VBox named vbox to hold display of plans and prices
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(0, 20, 0 ,20));
+        
+        // Use a Label control to hold the text that will be displayed 
+        lblPlansAndPrices = new Label(
+        "Plans and Prices\n"
+                + "--------------------\n"
+                + "A =  0 GB $50.00\n"
+                + "B =  2 GB $60.00\n"
+                + "C =  4 GB $70.00\n"
+                + "D = 10 GB $90.00\n"
+                + "+ $15.00/GB over plan limit"
+        );
+        
+        // Put label in the VBox
+        vbox.getChildren().add(lblPlansAndPrices);
+        
+        return vbox;
+    }   // End of createLeft
 }   // End mainFX
